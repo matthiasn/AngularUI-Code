265e251fb5023d60b3237978890e0cc748bba10d
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-10-31 12:14:04 +0100

    mask, bootstrap

--- Chapter02/Step02-Mask/src/js/controllers.js
+++ Chapter02/Step02-Mask/src/js/controllers.js
@@ -4,8 +4,7 @@ angular.module('myApp.controllers', []).controller('helloWorldCtrl', function ($
         first: "Jane",
         last: "Doe"
     };
-    //$scope.mask = "(999) 999-9999 ext 99";
-    $scope.mask = "Line: 99 Col: AA";
+    $scope.mask = "(999) 999-9999 ext 99";
     $scope.getModel = function () {
         return JSON.stringify($scope.name, undefined, 2);
     };

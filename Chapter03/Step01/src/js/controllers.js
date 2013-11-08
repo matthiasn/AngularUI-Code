c75f0231baa5c25e93b601cfacbb3a706eabade9
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-11-08 13:42:11 +0100

    ui-modules modification

--- Chapter03/Step01/src/js/controllers.js
+++ Chapter03/Step01/src/js/controllers.js
@@ -1,40 +1,6 @@
 'use strict';
-angular.module('myApp.controllers', []).controller('helloWorldCtrl', function ($scope, $timeout) {
-    $scope.name = {
-        first: "Jane",
-        last: "Doe"
-    };
-    $scope.mask = "(999) 999-9999 ext 99";
-    $scope.getModel = function () {
-        return JSON.stringify($scope.name, undefined, 2);
-    };
+angular.module('myApp.controllers', []).controller('uiModulesCtrl', function ($scope, $timeout) {
 
-    var colors = ["#CCC", "#F77", "#9F9"];
-    var activeColor = 0;
 
-    $scope.modelStatus = function() {
-        return { backgroundColor: colors[activeColor] };
-    } ;
 
-    $scope.focusCallback = function() {
-        activeColor = 1;
-    };
-
-    $scope.blurCallback = function() {
-        activeColor = 2;
-        $timeout(function() { activeColor = 0; }, 2000);
-    };
-
-    $scope.helpKeyDown = function($event) {
-        console.log($event);
-        $scope.helpText = "Easy. Just enter your name.";
-        $timeout(function() { $scope.helpText = "" }, 10000);
-    };
-
-    $scope.tooltip = function() {
-        if(!$scope.name.hasOwnProperty("phone")) {
-            return $scope.name.first + " has no phone?"
-        }
-        else { return "All good."}
-    }
 });

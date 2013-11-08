97e61b24ba6a10092ba6e139f7874bf21a8dcf13
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-11-08 15:22:18 +0100

    initial bower config

--- Chapter03/Step02-Maps/src/js/controllers.js
+++ Chapter03/Step02-Maps/src/js/controllers.js
@@ -1,6 +1,35 @@
 'use strict';
 angular.module('myApp.controllers', []).controller('uiModulesCtrl', function ($scope, $timeout) {
 
+    $scope.myMarkers = [];
 
+    $scope.mapOptions = {
+        center: new google.maps.LatLng(35.784, -78.670),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    $scope.addMarker = function($event) {
+        $scope.myMarkers.push(new google.maps.Marker({
+            map: $scope.myMap,
+            position: $params[0].latLng
+        }));
+    };
+
+    $scope.setZoomMessage = function(zoom) {
+        $scope.zoomMessage = 'You just zoomed to '+zoom+'!';
+        console.log(zoom,'zoomed')
+    };
+
+    $scope.openMarkerInfo = function(marker) {
+        $scope.currentMarker = marker;
+        $scope.currentMarkerLat = marker.getPosition().lat();
+        $scope.currentMarkerLng = marker.getPosition().lng();
+        $scope.myInfoWindow.open($scope.myMap, marker);
+    };
+
+    $scope.setMarkerPosition = function(marker, lat, lng) {
+        marker.setPosition(new google.maps.LatLng(lat, lng));
+    };
 
 });

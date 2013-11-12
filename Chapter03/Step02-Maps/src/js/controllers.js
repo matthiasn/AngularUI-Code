5a25287be29628e837bb3a5a8dbf5f0ed258767c
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-11-12 01:21:34 +0100

    weather map

--- Chapter03/Step02-Maps/src/js/controllers.js
+++ Chapter03/Step02-Maps/src/js/controllers.js
@@ -5,14 +5,11 @@ angular.module('myApp.controllers', []).controller('MapCtrl', function ($scope, 
     $scope.eventBinding = {'map-click': 'addMarker($event, $params)'};
 
     $scope.mapOptions = {
-        center: new google.maps.LatLng(35.784, -78.670),
+        center: new google.maps.LatLng(37.782,-122.418),
         zoom: 4,
         mapTypeId: google.maps.MapTypeId.SATELLITE
     };
 
-    var weatherLayer = new google.maps.weather.WeatherLayer({
-        temperatureUnits: google.maps.weather.TemperatureUnit.FAHRENHEIT
-    });
     var cloudLayer = new google.maps.weather.CloudLayer();
 
     $scope.addMarker = function($event, $params) {
@@ -22,13 +19,8 @@ angular.module('myApp.controllers', []).controller('MapCtrl', function ($scope, 
         }));
     };
 
-    $scope.setZoomMessage = function(zoom) {
-        $scope.zoomMessage = 'You just zoomed to '+zoom+'!';
-        console.log(zoom,'zoomed')
-    };
-
     $timeout(function(){
-        weatherLayer.setMap($scope.myMap);
         cloudLayer.setMap($scope.myMap);
-    }, 2000);
+    }, 1000);
+
 });

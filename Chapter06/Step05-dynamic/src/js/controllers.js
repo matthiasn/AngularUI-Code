5b151dc83d5014c647e763dcab231f7778b2aa6f
codef0rmer <amit.2006.it@gmail.com>
2014-03-23 20:24:54 +0530

    Dashboard for angular github repo to see top 100 issues open and PRs closed

--- Chapter06/Step05-dynamic/src/js/controllers.js
+++ Chapter06/Step05-dynamic/src/js/controllers.js
@@ -35,4 +35,37 @@ angular.module('myApp.controllers', []).controller('IssueCtrl', function ($scope
 
     $scope.data.data(data);
   });
+}).controller('PullCtrl', function ($scope, $http) {
+  $http.get('https://api.github.com/repos/angular/angular.js/pulls?state=closed&page=1&per_page=100&sort=updated&direction=desc').success(function(pulls) {
+    var pulley = [], data = [];
+
+    pulls.forEach(function(pull) {
+      pulley.push({value: new Date(pull.created_at).getDay()});
+    });
+    pulley = _.groupBy(pulley, function(pull) { return pull.value; });
+    pulley = _.sortBy(pulley, function(pull, i) { return pull[0].value; });
+    pulley.forEach(function(pull) {
+      data.push({value: pull.length});
+    });
+
+    $scope.data.data(data);
+  });
+
+  $scope.column = {
+    legend: {
+      visible: false
+    },
+    valueAxis: {
+      max: 100
+    },
+    categoryAxis: {
+      categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat']
+    },
+    series: [{
+      type: "area",
+      field: "value"
+    }]
+  };
+
+  $scope.data = new kendo.data.DataSource({});
 });

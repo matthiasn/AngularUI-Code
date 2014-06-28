e4428bde5f33011f9f54f40edb724b224c77137e
codef0rmer <amit.2006.it@gmail.com>
2014-06-28 16:48:38 +0530

    Chapter 09: minor typos in step02-tab

--- Chapter09/Step02-tabs/src/js/controllers.js
+++ Chapter09/Step02-tabs/src/js/controllers.js
@@ -2,15 +2,9 @@ angular.module('myApp.controllers', []).controller('AdvanceTabCtrl', function($s
   // Alternatively use $parent.$parent.$parent.close but not necessary 
   // because the scope is prototypically inherited
   $templateCache.put("template/tabs/tab.html",
-    // '<li ng-class="{active: active, disabled: disabled}">\
-    //   <a ng-click="select()" tab-heading-transclude>\
-    //     <button type="button" class="close" ng-click="$parent.close($parent.$index)" ng-style="{true: {visibility: \'hidden\'}}[!active]">&times;</button>\
-    //     {{heading}}\
-    //   </a>\
-    // </li>'  
     '<li ng-class="{true: {active: active, disabled: disabled}}[heading != \'new_tab\']">\
       <a ng-show="heading != \'new_tab\'" ng-click="select()" tab-heading-transclude>\
-       < button type="button" class="close" ng-click="$parent.close($parent.$index)" ng-style="{true: {visibility: \'hidden\'}}[!active || $parent.tab.closeable == false]">&times;</button>\
+       <button type="button" class="close" ng-click="$parent.close($parent.$index)" ng-style="{true: {visibility: \'hidden\'}}[!active || $parent.tab.closeable == false]">&times;</button>\
         {{heading}}\
       </a>\
       <a ng-show="heading == \'new_tab\'" ng-click="$parent.add()">\

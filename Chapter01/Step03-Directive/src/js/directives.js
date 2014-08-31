785c5fd9f71e0d6c5a64990d29f98b35d78413c7
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-08-31 12:47:25 +0200

    replacing "data-ng-" with "ng-"

--- Chapter01/Step03-Directive/src/js/directives.js
+++ Chapter01/Step03-Directive/src/js/directives.js
@@ -5,7 +5,7 @@ angular.module('myApp.directives', [])
         restrict: 'AE',
         scope: { name: "=name" },
         template: "<h1>Hello {{ name.first }} {{ name.last }}!</h1>" +
-                  "<input data-ng-model='name.first'></input>" + 
-                  "<input data-ng-model='name.last'></input>"
+                  "<input ng-model='name.first'></input>" + 
+                  "<input ng-model='name.last'></input>"
         }
     });
